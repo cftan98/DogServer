c2e3dddb2ebc21f180ea0c2ca5dc05d9f6d44dfc
cftan98 <cftan98@users.noreply.github.com>
2021-06-11 15:25:19 +1200

    Refactor the authentication method

--- AuthStrategies/twitterStrategy.js
+++ AuthStrategies/twitterStrategy.js
@@ -19,49 +19,44 @@ const twitterStrategy = new TwitterStrategy(
         });
 
         router.get('/isfollowing/:name', (req, res) => {
-            if (req.isAuthenticated()) {
+            if (!req.isAuthenticated()) { throw new Error("Please login!") }
+
+            try {
                 T.get("friendships/show", { source_screen_name: req.params.name, target_screen_name: "BitDotCountry" },
                     (err, data, response) => {
-                        try {
-                            if (data.relationship.source.following && data.relationship.target.followed_by) {
-                                T.get("friendships/show", { source_screen_name: req.params.name, target_screen_name: "RayLuCode" },
-                                    (err, data, response) => {
-                                        if (data.relationship.source.following && data.relationship.target.followed_by) {
-                                            res.end(JSON.stringify({ "Is_success": true, "Is_following": true }));
-                                        } else {
-                                            res.end(JSON.stringify({ "Is_success": true, "Is_following": false }));
-                                        }
-                                    });
-                            } else {
-                                res.end(JSON.stringify({ "Is_success": true, "Is_following": false }));
-                            }
-                        } catch (error) { // Connection false for input not correct
-                            res.end(JSON.stringify({ "Is_success": false }));
+                        if (data.relationship.source.following && data.relationship.target.followed_by) {
+                            T.get("friendships/show", { source_screen_name: req.params.name, target_screen_name: "RayLuCode" },
+                                (err, data, response) => {
+                                    if (data.relationship.source.following && data.relationship.target.followed_by) {
+                                        res.end(JSON.stringify({ "Is_success": true, "Is_following": true }));
+                                    } else {
+                                        res.end(JSON.stringify({ "Is_success": true, "Is_following": false }));
+                                    }
+                                });
+                        } else {
+                            res.end(JSON.stringify({ "Is_success": true, "Is_following": false }));
                         }
                     });
-            } else {
-                res.status(403).send();
+            } catch (err) {
+                res.end(JSON.stringify({ "Is_success": false, "Message": err }));
             }
         });
 
         router.get('/isLikedAndRetweeted/:name', (req, res) => {
-            if (req.isAuthenticated()) {
-                try {
-                    // The post id is between since_id and max_id
-                    T.get("favorites/list", { source_screen_name: req.params.name, since_id: "1380385038320115714", max_id: "1380385038320115716" },
-                        (err, data, response) => {
-                            if (data.length === 1 && data[0].retweeted) {
-                                res.end(JSON.stringify({ "Is_success": true, "Is_retweeted_and_liked": true }));
-                            } else {
-                                res.end(JSON.stringify({ "Is_success": true, "Is_retweeted_and_liked": false }));
-                            }
+            if (!req.isAuthenticated()) { throw new Error("Please login!") }
 
-                        });
-                } catch (error) { // Connection false for input not correct
-                    res.end(JSON.stringify({ "Is_success": false }));
-                }
-            } else {
-                res.status(403).send();
+            try {
+                // The post id is between since_id and max_id
+                T.get("favorites/list", { source_screen_name: req.params.name, since_id: "1380385038320115714", max_id: "1380385038320115716" },
+                    (err, data, response) => {
+                        if (data.length === 1 && data[0].retweeted) {
+                            res.end(JSON.stringify({ "Is_success": true, "Is_retweeted_and_liked": true }));
+                        } else {
+                            res.end(JSON.stringify({ "Is_success": true, "Is_retweeted_and_liked": false }));
+                        }
+                    });
+            } catch (error) { // Connection false for input not correct
+                res.end(JSON.stringify({ "Is_success": false, "Message": error }));
             }
         });
 
